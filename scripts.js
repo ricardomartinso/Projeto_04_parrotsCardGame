8605a2dadcd864dbf77bca00b6570c38c82be0f9
Ricardo <ricardomartinso.dev@gmail.com>
2022-04-08 11:58:37 -0300

    SetTimeOut implementado na função esperarCarta que só roda se as cartas forem diferentes e adição da função que finaliza o jogo e conta as rodadas(por click)

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let cartasArray;
+let arrayTeste;
 let parrotCard;
 let parrotCardFront;
 let parrotCardBack;
@@ -19,33 +21,40 @@ let gifsArray = ["./images/bobrossparrot.gif",
 "./images/unicornparrot.gif",
 "./images/unicornparrot.gif"
 ];
-
+let rodadas = [];
 let viradas = document.querySelectorAll(".virada");
+let perguntaCartas;
 
-pickCards();
+pickCards(); // Prompt Qtde Cartas + Random das cartas 
 
 
 function virarCarta(carta) {
+        rodadas.push(carta);
+        cartasArray = document.querySelectorAll(".virar-carta-back"); //todas as cartas que foram viradas
 
-        let cartasArray = document.querySelectorAll(".virar-carta-back");
-        let parrotCardFront = carta.children[0];
-        let parrotCardBack = carta.children[1];
+        console.log(carta)
+        arrayTeste = Array.from(cartasArray);
+        let parrotCardFront = carta.children[0]; //div parte frontal da carta
+        let parrotCardBack = carta.children[1]; //div parte traseira da carta
+
 
 
         if (cartasViradas.length < 2) {
-
-        parrotCardBack.classList.add("virar-carta-back");
-        cartasViradas.push(carta);
+        parrotCardBack.classList.add("virar-carta-back"); //Efeito de virar a carta e mostrar a parte traseira
+        cartasViradas.push(carta); // PUSH para aumentar tamanho do length para só permitir virar até 2 cartas por vez
         
     }
     compararCartas();
+    
+
 }
 
 function compararCartas() {
-    let cartasArray = document.querySelectorAll(".virar-carta-back");
-
     
-    if (cartasArray[0].querySelector("img").attributes.src.value === cartasArray[1].querySelector("img").attributes.src.value) {
+    cartasArray = document.querySelectorAll(".virar-carta-back");
+    arrayTeste = Array.from(cartasArray);
+
+    if (arrayTeste[0].querySelector("img").attributes.src.value === arrayTeste[1].querySelector("img").attributes.src.value) {
 
         for (let i = 0; i < 2; i++) {
             cartasArray[i].classList.add("carta-igual");
@@ -53,26 +62,35 @@ function compararCartas() {
             cartasArray[i].classList.remove("face");
             cartasArray[i].previousElementSibling.classList.add("hidden");
             cartasArray[i].parentNode.classList.remove("virada");
+            cartasArray[i].parentNode.classList.add("certas");
         }
-        cartasArray = [];
+        arrayTeste.pop();
+        arrayTeste.pop();
+        gameOver();
     } 
-    
-    else if (cartasArray[0].querySelector("img").attributes.src.value !== cartasArray[1].querySelector("img").attributes.src.value) {
-        for (let i = 0; i < 2; i++) {
-            cartasArray[i].classList.remove("virar-carta-back");
-
-        }
+    else if (arrayTeste[0].querySelector("img").attributes.src.value !== arrayTeste[1].querySelector("img").attributes.src.value) {
+        setTimeout(esperarCarta, 1000);
     }
-        cartasViradas = [];
-}
+    cartasViradas = [];
+    
 
+}
+function esperarCarta() {
+        cartasArray = document.querySelectorAll(".virar-carta-back");
+        arrayTeste = Array.from(cartasArray);
+        for (let i = 0; i < 2; i++) {
+        cartasArray[i].classList.remove("virar-carta-back");
+    }
+    arrayTeste.pop();
+    arrayTeste.pop();
+}
 
 function pickCards() {
     let lista = document.querySelector(".parrot-cards");
     
-    let perguntaCartas = prompt("Quantas cartas quer jogar?");
+    perguntaCartas = prompt("Quantas cartas quer jogar?");
     let gifs = document.querySelector(".parrot-card-back img");
-    let arrayTeste = [];
+    arrayTeste = [];
 
     if (perguntaCartas % 2 == 0 && perguntaCartas >= 4 && perguntaCartas <= 14) {
     for (let i = 0; i < perguntaCartas; i++) {
@@ -96,4 +114,9 @@ function pickCards() {
 }
 function comparador() {
     return Math.random() - 0.5;
+}
+function gameOver() {
+    if (document.querySelectorAll(".parrot-card.certas").length === parseInt(perguntaCartas)) {
+        alert(`Você ganhou em ${rodadas.length} rodadas`);
+    }
 }
