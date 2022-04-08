8bb72783a0eb1571b2d2bd7580abade7f5edd227
Ricardo <ricardomartinso.dev@gmail.com>
2022-04-07 23:05:29 -0300

    Ajustes na comparação

--- scripts.js
+++ scripts.js
@@ -20,62 +20,52 @@ let gifsArray = ["./images/bobrossparrot.gif",
 "./images/unicornparrot.gif"
 ];
 
-pickCards();
+let viradas = document.querySelectorAll(".virada");
 
+pickCards();
 
 
 function virarCarta(carta) {
 
         let cartasArray = document.querySelectorAll(".virar-carta-back");
-        let viradas = document.querySelectorAll(".viradas");
         let parrotCardFront = carta.children[0];
         let parrotCardBack = carta.children[1];
+
 
         if (cartasViradas.length < 2) {
 
         parrotCardBack.classList.add("virar-carta-back");
         cartasViradas.push(carta);
-        console.log(cartasViradas.length)
+        
     }
     compararCartas();
 }
-// cartasArray[0].querySelector("img").attributes.src.value
+
 function compararCartas() {
     let cartasArray = document.querySelectorAll(".virar-carta-back");
+
     
     if (cartasArray[0].querySelector("img").attributes.src.value === cartasArray[1].querySelector("img").attributes.src.value) {
 
         for (let i = 0; i < 2; i++) {
             cartasArray[i].classList.add("carta-igual");
+            cartasArray[i].classList.add("virar-carta-back");
+            cartasArray[i].classList.remove("face");
+            cartasArray[i].previousElementSibling.classList.add("hidden");
             cartasArray[i].parentNode.classList.remove("virada");
         }
-
+        cartasArray = [];
     } 
     
     else if (cartasArray[0].querySelector("img").attributes.src.value !== cartasArray[1].querySelector("img").attributes.src.value) {
         for (let i = 0; i < 2; i++) {
             cartasArray[i].classList.remove("virar-carta-back");
-            cartasArray[i].parentNode.classList.remove("selecionada");
+
         }
     }
         cartasViradas = [];
 }
 
- 
-/*function compararCartasDiferentes() {
-    compararGif = document.querySelectorAll(".parrot-card-back.selecionada");
-
-    if (compararGif[0].attributes.src.value !== compararGif[1].attributes.src.value) {
-        compararGif[0].classList.add("escondido");
-        compararGif[0].parentNode.querySelector("img").classList.remove("escondido");
-        compararGif[0].classList.remove("selecionada");
-        compararGif[1].classList.add("escondido");
-        compararGif[1].parentNode.querySelector("img").classList.remove("escondido");
-        compararGif[1].classList.remove("selecionada");
-        cartasViradas = [];
-    }
-}
-*/
 
 function pickCards() {
     let lista = document.querySelector(".parrot-cards");
